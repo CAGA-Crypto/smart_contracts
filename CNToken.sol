dcb221e261014c9fa22c08382f2652fb17d4ba33
skcaga <161133869+skcaga@users.noreply.github.com>
2024-04-15 13:36:30 +0300

    Add files via upload

--- CNToken.sol
+++ CNToken.sol
@@ -1,22 +1,29 @@
 // SPDX-License-Identifier: MIT
-// Compatible with OpenZeppelin Contracts ^5.0.0
 pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 
-contract CNToken is ERC20, Ownable, ERC20Permit {
-    constructor(address initialOwner)
+contract CNToken is ERC20, AccessControl, ERC20Permit {
+    bytes32 public constant BURNER_ROLE = keccak256("BURNER_ROLE");
+
+    constructor()
         ERC20("CAGA N Token", "CNT")
-        Ownable(initialOwner)
         ERC20Permit("CAGA N Token")
     {
+        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
         _mint(msg.sender, 100000 * 10**decimals());
     }
 
-    function mint(address to, uint256 amount) public onlyOwner {
+    function mint(address to, uint256 amount) public {
+        require(hasRole(DEFAULT_ADMIN_ROLE, msg.sender), "Caller is not a minter");
         _mint(to, amount);
+    }
+
+    function burn(address from, uint256 amount) public {
+        require(hasRole(BURNER_ROLE, msg.sender), "Caller is not a burner");
+        _burn(from, amount);
     }
 
     function transfer(address to, uint256 amount)
@@ -36,5 +43,10 @@ contract CNToken is ERC20, Ownable, ERC20Permit {
     ) public virtual override returns (bool) {
         require(false, "Transfer is disabled");
         return false;
+    }
+
+    function grantRole(bytes32 role, address account) public override {
+        require(hasRole(DEFAULT_ADMIN_ROLE, msg.sender), "Caller is not an admin");
+        super.grantRole(role, account);
     }
 }
