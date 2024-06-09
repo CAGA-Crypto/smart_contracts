1fe8753fbb00a092850ba6bb2f4f9f93ba2422da
skcaga <semyon@cagacrypto.com>
2024-06-09 18:43:58 +0300

    upd

--- pump/InternalSwap.sol
+++ pump/InternalSwap.sol
@@ -1,29 +1,34 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.18;
+pragma solidity ^0.8.26;
 
-import "@openzeppelin/contracts@4.9.0/access/Ownable.sol";
-import "@openzeppelin/contracts@4.9.0/token/ERC20/utils/SafeERC20.sol";
-import "@openzeppelin/contracts@4.9.0/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "@uniswap/v2-periphery/contracts/interfaces/IUniswapV2Router02.sol";
 import "@uniswap/v2-core/contracts/interfaces/IUniswapV2Factory.sol";
+import "./IWETH9.sol";
 
 contract InternalSwap is Ownable, ReentrancyGuard {
     IERC20 public userToken;
-    IERC20 public weth;
+    IWETH9 public weth;
     IUniswapV2Router02 public uniswapRouter;
     address public uniswapPair;
 
     uint256 public reserveWeth;
     uint256 public reserveUserToken;
+    uint256 public initialSupply;
 
     uint256 public feeBps;
     uint256 public minUserToken;
+    uint256 public hardCap;
+
+    mapping(address => uint256) public userTokenBalance;
 
     enum Reserve {WETH, UserToken}
 
     event AddLiquidity(Reserve typeReserve, uint256 value);
     event RemoveLiquidity(Reserve typeReserve, uint256 value);
-    event Swap(uint256 weth, uint256 userToken, uint256 price, uint256 fee);
+    event Swap(uint256 weth, uint256 userToken, uint256 price, uint256 fee, address tokenAddress, address from);
     event TokenListed(address pair);
 
     constructor(
@@ -31,21 +36,25 @@ contract InternalSwap is Ownable, ReentrancyGuard {
         address _weth,
         address _uniswapRouter,
         uint256 _feeBps,
-        uint256 _minUserToken
-    ) {
+        uint256 _minUserToken,
+        uint256 _hardCap,
+        uint256 _initialSupply
+    ) Ownable(msg.sender) {
         userToken = IERC20(_userToken);
-        weth = IERC20(_weth);
+        weth = IWETH9(_weth);
         uniswapRouter = IUniswapV2Router02(_uniswapRouter);
         feeBps = _feeBps;
         minUserToken = _minUserToken;
+        hardCap = _hardCap;
+        reserveUserToken = _initialSupply;
     }
 
     receive() external payable {}
     fallback() external payable {}
 
     function swapWethToUserToken(uint256 _wethIn) external nonReentrant {
+        require(uniswapPair == address(0), "Token already listed on Uniswap");
         require(_wethIn != 0, "Pay WETH to get UserToken");
-        require(weth.allowance(msg.sender, address(this)) >= _wethIn, "WETH allowance too low");
 
         (uint256 _outputUserToken, uint256 _price) = wethOverUserTokenValueAndPrice(0, _wethIn);
 
@@ -55,25 +64,19 @@ contract InternalSwap is Ownable, ReentrancyGuard {
         if (swFee < minUserToken) {
             swFee = minUserToken;
         }
-
         require(swFee <= _outputUserToken, "Fee more than output value");
 
-        SafeERC20.safeTransferFrom(weth, msg.sender, address(this), _wethIn);
-        SafeERC20.safeTransfer(userToken, msg.sender, (_outputUserToken - swFee));
+        weth.transferFrom(msg.sender, address(this), _wethIn);
+        userTokenBalance[msg.sender] += (_outputUserToken - swFee);
         reserveWeth += _wethIn;
         reserveUserToken -= _outputUserToken;
 
-        emit Swap(_wethIn, _outputUserToken, _price, swFee);
-
-        // Check if all user tokens are bought out
-        if (reserveUserToken == 0 && uniswapPair == address(0)) {
-            listToken(reserveWeth, reserveUserToken);
-        }
+        emit Swap(_wethIn, _outputUserToken, _price, swFee, address(userToken), msg.sender);
     }
 
     function swapUserTokenToWeth(uint256 _userTokenIn) external nonReentrant {
+        require(uniswapPair == address(0), "Token already listed on Uniswap");
         require(_userTokenIn != 0, "Pay UserToken to get WETH");
-        require(userToken.allowance(msg.sender, address(this)) >= _userTokenIn, "UserToken allowance too low");
 
         uint256 swFee = calculate(_userTokenIn);
         if (swFee < minUserToken) {
@@ -88,12 +91,19 @@ contract InternalSwap is Ownable, ReentrancyGuard {
         require(_outputWeth <= weth.balanceOf(address(this)), "No liquidity");
 
         SafeERC20.safeTransferFrom(userToken, msg.sender, address(this), _userTokenIn);
-        SafeERC20.safeTransfer(weth, msg.sender, _outputWeth);
+        weth.transfer(msg.sender, _outputWeth);
 
         reserveUserToken += userTokenInWithFee;
         reserveWeth -= _outputWeth;
 
-        emit Swap(_outputWeth, _userTokenIn, _price, swFee);
+        emit Swap(_outputWeth, _userTokenIn, _price, swFee, address(userToken), msg.sender);
+    }
+
+    function mintUserToken() external nonReentrant {
+        uint256 amount = userTokenBalance[msg.sender];
+        require(amount > 0, "No user token balance to mint");
+        userTokenBalance[msg.sender] = 0;
+        userToken.transfer(msg.sender, amount);
     }
 
     function setMinBps(uint256 _newBps) external onlyOwner {
@@ -104,14 +114,15 @@ contract InternalSwap is Ownable, ReentrancyGuard {
         minUserToken = _newUserToken;
     }
 
-    function addWethReserve(uint256 _wethIn) external onlyOwner {
-        require(weth.allowance(msg.sender, address(this)) >= _wethIn, "WETH allowance too low");
-        SafeERC20.safeTransferFrom(weth, msg.sender, address(this), _wethIn);
+    function addWethReserve(uint256 _wethIn) external payable onlyOwner {
+        require(uniswapPair == address(0), "Token already listed on Uniswap");
+        weth.transferFrom(msg.sender, address(this), _wethIn);
         reserveWeth += _wethIn;
         emit AddLiquidity(Reserve.WETH, _wethIn);
     }
 
     function addUserTokenReserve(uint256 _userTokenIn) external onlyOwner {
+        require(uniswapPair == address(0), "Token already listed on Uniswap");
         require(userToken.allowance(msg.sender, address(this)) >= _userTokenIn, "UserToken allowance too low");
         SafeERC20.safeTransferFrom(userToken, msg.sender, address(this), _userTokenIn);
         reserveUserToken += _userTokenIn;
@@ -120,7 +131,7 @@ contract InternalSwap is Ownable, ReentrancyGuard {
 
     function removeWethReserve(uint256 _wethIn, bool _rebalance) external onlyOwner {
         require(_wethIn <= weth.balanceOf(address(this)), "No liquidity");
-        SafeERC20.safeTransfer(weth, msg.sender, _wethIn);
+        weth.withdraw(_wethIn);
         if (_rebalance) {
             reserveWeth -= _wethIn;
             emit RemoveLiquidity(Reserve.WETH, _wethIn);
@@ -168,11 +179,12 @@ contract InternalSwap is Ownable, ReentrancyGuard {
         return amount * feeBps / 10_000;
     }
 
-    function listToken(uint256 _wethAmount, uint256 _userTokenAmount) internal {
+    function listToken(uint256 _wethAmount, uint256 _userTokenAmount, uint256 _amountAMin, uint256 _amountBMin) public onlyOwner {
         require(uniswapPair == address(0), "Token already listed on Uniswap");
 
-        SafeERC20.safeApprove(weth, address(uniswapRouter), _wethAmount);
-        SafeERC20.safeApprove(userToken, address(uniswapRouter), _userTokenAmount);
+        weth.approve(address(uniswapRouter), _wethAmount);
+
+        userToken.approve(address(uniswapRouter), _userTokenAmount);
 
         // Add liquidity to Uniswap
         (uint256 amountWeth, uint256 amountUserToken, uint256 liquidity) = uniswapRouter.addLiquidity(
@@ -180,12 +192,11 @@ contract InternalSwap is Ownable, ReentrancyGuard {
             address(userToken),
             _wethAmount,
             _userTokenAmount,
-            0,
-            0,
+            _amountAMin,
+            _amountBMin,
             owner(),
             block.timestamp
         );
-
 
         address pair = IUniswapV2Factory(uniswapRouter.factory()).getPair(address(weth), address(userToken));
         require(pair != address(0), "Uniswap pair creation failed");
--- pump/PumpFactory.sol
+++ pump/PumpFactory.sol
@@ -1,62 +1,128 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.18;
+pragma solidity ^0.8.26;
 
-import "@openzeppelin/contracts@4.9.0/access/Ownable.sol";
-import "@openzeppelin/contracts@4.9.0/token/ERC20/IERC20.sol";
-import "@openzeppelin/contracts@4.9.0/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "./InternalSwap.sol";
 import "./UserToken.sol";
+import "./IWETH9.sol";
 
 contract PumpFactory is Ownable {
     address public weth;
     address public uniswapRouter;
+    mapping(address => address) public mapTokensToSwap;
 
-    event InternalSwapDeployed(address indexed swapContract, address indexed userToken, address indexed owner);
-    event LiquidityProvided(address indexed swapContract, uint256 wethAmount, uint256 userTokenAmount);
-    event TokenDeployed(address indexed tokenAddress, string name, string symbol, uint256 initialSupply);
+    event InternalSwapDeployed(
+        address indexed swapContract,
+        address indexed userToken,
+        address indexed owner
+    );
+    event LiquidityProvided(
+        address indexed swapContract,
+        uint256 wethAmount,
+        uint256 userTokenAmount
+    );
+    event TokenDeployed(
+        address indexed tokenAddress,
+        string name,
+        string symbol,
+        uint256 initialSupply,
+        string twitter,
+        string telegram,
+        string website,
+        string imageUri,
+        address from
+    );
 
-    constructor(address _weth, address _uniswapRouter) {
+    error AllowanceTooLow(address token, uint256 required, uint256 available);
+
+    constructor(address _weth, address _uniswapRouter) Ownable(msg.sender) {
         weth = _weth;
         uniswapRouter = _uniswapRouter;
+    }
+
+    function deployToken(
+        string memory _tokenName,
+        string memory _tokenSymbol,
+        uint256 _initialSupply
+    ) internal returns (UserToken) {
+        UserToken newToken = new UserToken(
+            _tokenName,
+            _tokenSymbol,
+            _initialSupply
+        );
+        return newToken;
+    }
+
+    function deployInternalSwap(
+        address tokenAddress,
+        uint256 _feeBps,
+        uint256 _minUserToken,
+        uint256 _hardCap,
+        uint256 _initialSupply
+    ) internal returns (InternalSwap) {
+        InternalSwap newSwap = new InternalSwap(
+            tokenAddress,
+            weth,
+            uniswapRouter,
+            _feeBps,
+            _minUserToken,
+            _hardCap,
+            _initialSupply
+        );
+        return newSwap;
     }
 
     function deployTokenAndInternalSwap(
         string memory _tokenName,
         string memory _tokenSymbol,
+        string memory _twitter,
+        string memory _telegram,
+        string memory _website,
+        string memory _imageUri,
         uint256 _initialSupply,
         uint256 _feeBps,
-        uint256 _minUserToken
+        uint256 _minUserToken,
+        uint256 _hardCap
     ) external onlyOwner returns (address, address) {
         // Deploy new ERC20 token
-        UserToken newToken = new UserToken(_tokenName, _tokenSymbol, _initialSupply);
-        emit TokenDeployed(address(newToken), _tokenName, _tokenSymbol, _initialSupply);
+        UserToken newToken = deployToken(
+            _tokenName,
+            _tokenSymbol,
+            _initialSupply
+        );
+        emit TokenDeployed(
+            address(newToken),
+            _tokenName,
+            _tokenSymbol,
+            _initialSupply,
+            _twitter,
+            _telegram,
+            _website,
+            _imageUri,
+            msg.sender
+        );
 
         // Deploy new InternalSwap contract
-        InternalSwap newSwap = new InternalSwap(address(newToken), weth, uniswapRouter, _feeBps, _minUserToken);
+        InternalSwap newSwap = deployInternalSwap(
+            address(newToken),
+            _feeBps,
+            _minUserToken,
+            _hardCap,
+            _initialSupply
+        );
         newSwap.transferOwnership(msg.sender);
-        emit InternalSwapDeployed(address(newSwap), address(newToken), msg.sender);
+        emit InternalSwapDeployed(
+            address(newSwap),
+            address(newToken),
+            msg.sender
+        );
 
+        newToken.approve(address(this), _initialSupply);
+        newToken.approve(address(newSwap), _initialSupply);
+        newToken.transferFrom(address(this), address(newSwap), _initialSupply);
+        mapTokensToSwap[address(newToken)] = address(newSwap);
         return (address(newToken), address(newSwap));
-    }
-
-    function provideLiquidity(
-        address payable _swapContract,
-        uint256 _wethAmount,
-        uint256 _userTokenAmount
-    ) external onlyOwner {
-        InternalSwap swapContract = InternalSwap(_swapContract);
-        IERC20 userToken = IERC20(swapContract.userToken());
-        IERC20 wethToken = IERC20(swapContract.weth());
-
-        require(userToken.allowance(msg.sender, address(this)) >= _userTokenAmount, "User token allowance too low");
-        require(wethToken.allowance(msg.sender, address(this)) >= _wethAmount, "WETH allowance too low");
-
-        SafeERC20.safeTransferFrom(userToken, msg.sender, address(swapContract), _userTokenAmount);
-        SafeERC20.safeTransferFrom(wethToken, msg.sender, address(swapContract), _wethAmount);
-
-        swapContract.addUserTokenReserve(_userTokenAmount);
-        swapContract.addWethReserve(_wethAmount);
-
-        emit LiquidityProvided(address(swapContract), _wethAmount, _userTokenAmount);
     }
 }
